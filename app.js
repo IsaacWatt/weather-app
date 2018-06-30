37c1c8af6e5ca83a5237be02d2aa67d20898eb27
Isaac Watt <isaacwatt@icloud.com>
2018-06-29 21:59:55 -0400

    handles callback errors

--- app.js
+++ app.js
@@ -19,8 +19,16 @@ const argv = yargs
 
 
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}',
+	url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-
+	if (error) { // if the error object exists
+		console.log('Unable to connect to the Google servers');
+	} else if (body.status === 'ZERO_RESULTS') {
+		console.log('Unable to find that address');
+	} else if (body.status === 'OK') {
+		console.log(`Address: ${body.results[0].formatted_address}`);
+		console.log(`Address: ${body.results[0].geometry.location.lat}`);
+		console.log(`Address: ${body.results[0].geometry.location.lng}`);
+	}
 });
